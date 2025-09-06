ad2e200879461ee5de918657d9f348b9545f609a
dev-sajjadhosan <devsajjadhosan@gmail.com>
2025-09-06 16:08:51 +0600

    Alhamdulillah! get-start page 2 tab component done next working

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const genPass = (len = 9) => {
+  const sets = [
+    'abcdefghijklmnopqrstuvwxyz',
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    '0123456789',
+    '!@#$%^&*_/|',
+  ]
+  let p = sets.map((s) => s[(Math.random() * s.length) | 0]).join('')
+  const all = sets.join('')
+  while (p.length < len) p += all[(Math.random() * all.length) | 0]
+  return [...p].sort(() => Math.random() - 0.5).join('')
 }
